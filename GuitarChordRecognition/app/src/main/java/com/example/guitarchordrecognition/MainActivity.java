73a9608b47eabfa30a032e5b4a3cfa0c717294c7
Saaketh Jasti <saakethkjasti@outlook.com>
2019-12-17 15:19:14 -0600

    First Page
    Working First page and ability to access second

--- GuitarChordRecognition/app/src/main/java/com/example/guitarchordrecognition/MainActivity.java
+++ GuitarChordRecognition/app/src/main/java/com/example/guitarchordrecognition/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.guitarchordrecognition;
 
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button welcome;
+        welcome = (Button) findViewById(R.id.welcomeButton);
+
+        welcome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.record);
+            }
+        });
+
     }
 }
